6bdb4f757eb5436fbd302ecea9d9b01b4d986ca0
Sysak <www.vantanphsh1036@gmail.com>
2018-06-20 00:37:28 +0900

    completed sln6_B

--- itp1/sln6_B.c
+++ itp1/sln6_B.c
@@ -3,69 +3,76 @@
 
 int main()
 {
-	int n = 0, *p;
-	char *c;
-	scanf("%d",&n);
-	if(n<=0) return 0;
-	p = (int *)calloc(n,sizeof(int));
-	c = (char *)calloc(n,sizeof(char));
-	if(p == NULL || c == NULL) return EXIT_FAILURE;
+	int in;
+	scanf("%d",&in);
+	if(in<=0) return 0;
 
-	int i;
-	int sp[13],he[13],cl[13],di[13];
-	for(i=0;i<13;i++) {
-		sp[i] = 0;
-		he[i] = 0;
-		cl[i] = 0;
-		di[i] = 0;
-	}
+	char suit[2];
+	int *S, *H, *C, *D;
+	S = (int *)calloc(13,sizeof(int));
+	H = (int *)calloc(13,sizeof(int));
+	C = (int *)calloc(13,sizeof(int));
+	D = (int *)calloc(13,sizeof(int));
+	if(S==NULL || H==NULL || C==NULL || D==NULL) return -1;
 
-	for(i=0;i<n;i++) {
-		scanf("%c %d",&c[i],&p[i]);
-	}
-
-	for(i=0;i<n;i++) {
-		printf("%c %d",c[i],p[i]);
-	}
-
-	/*int sp[13],he[13],cl[13],di[13];
-	for(i=0;i<13;i++) {
-		sp[i] = 0;
-		he[i] = 0;
-		cl[i] = 0;
-		di[i] = 0;
-	}*/
-
-	for(i=0;i<n;i++) {
-		switch(c[i]) {
-			case 'S': {
-				sp[p[i]-1]=1;
+	int i, rank;
+	for(i=0;i<in;i++) {
+		scanf("%2s %d",suit,&rank);
+		switch(suit[0]) {
+			case 'S':
+				S[rank-1] = 1;
 				break;
-			}
-			case 'H': {
-				he[p[i]-1]=1;
+			case 'H':
+				H[rank-1] = 1;
 				break;
-			}
-			case 'C': {
-				cl[p[i]-1]=1;
+			case 'C':
+				C[rank-1] = 1;
 				break;
-			}
-			case 'D': {
-				di[p[i]-1]=1;
+			case 'D':
+				D[rank-1] = 1;
 				break;
-			}
 		}
 	}
 
-	for(i=0;i<13;i++) {
-		if(!sp[i]) printf("S %d\n",i);
-		if(!he[i]) printf("H %d\n",i);
-		if(!cl[i]) printf("C %d\n",i);
-		if(!di[i]) printf("D %d\n",i);
+	int state = 0;
+	while(state!=4) {
+		for(i=0;i<13;i++) {
+			if(!S[i] && !state) {
+				printf("S %d\n",i+1);
+				S[i] = 1;
+			}
+			if(!H[i] && state==1) {
+				printf("H %d\n",i+1);
+				H[i] = 1;
+			}
+			if(!C[i] && state==2) {
+				printf("C %d\n",i+1);
+				C[i] = 1;
+			}
+			if(!D[i] && state==3) {
+				printf("D %d\n",i+1);
+				D[i] = 1;
+			}
+			if(i==12) {
+				if(S[i] && !state) {
+					state=1;
+					continue;
+				}
+				if(H[i] && state==1) {
+					state=2;
+					continue;
+				}
+				if(C[i] && state==2) {
+					state=3;
+					continue;
+				}
+				if(D[i] && state==3) {
+					state=4;
+					continue;
+				}
+			}
+		}
 	}
-
-	free(p);
-	free(c);
 
 	return 0;
 }
