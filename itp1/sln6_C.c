0b116cc2b828a7024e0db2ef20a18fef52b3c7ba
Sysak <www.vantanphsh1036@gmail.com>
2018-06-22 01:17:21 +0900

    Probably it was completed

--- itp1/sln6_C.c
+++ itp1/sln6_C.c
@@ -5,14 +5,28 @@ int main()
 	int in;
 	scanf("%d",&in);
 	
-	int b[in], f[in], r[in], v[in];
-	int i;
-	for(i=0;i<in;i++) {
-		scanf("%d%d%d%d", &b[i], &f[i], &r[i], &v[i]);
+	int b, f, r, v;
+	int tenant[4][3][10];
+	int i,j;
+
+	for(i=0;i<12;i++) {
+		for(j=0;j<10;j++) {
+			tenant[i/3][i%3][j] = 0;
+		}
 	}
 
 	for(i=0;i<in;i++) {
-		printf("%d, %d, %d, %d\n", b[i], f[i], r[i], v[i]);
+		scanf("%d%d%d%d", &b, &f, &r, &v);
+		tenant[b-1][f-1][r-1] = v;
 	}
+
+	for(i=0;i<12;i++) {
+		for(j=0;j<10;j++) {
+			printf(" %d",tenant[i/3][i%3][j]);
+		}
+		putchar('\n');
+		if(i>0&&i<10&&(i+1)%3==0)printf("####################\n");
+	}
+	
 	return 0;
 }
