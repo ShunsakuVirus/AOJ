76ebbc20538e78d4e5ae2711d35f2905588bffed
Sysak <www.vantanphsh1036@gmail.com>
2018-06-22 02:39:38 +0900

    Completed sln6

--- itp1/sln6_D.c
+++ itp1/sln6_D.c
@@ -2,5 +2,27 @@
 
 int main()
 {
+	int n,m;
+	scanf("%d%d", &n, &m);
+
+	int A[n][m], b[m];
+	int i;
+	for(i=0;i<n*m;i++) {
+		A[i/m][i%m] = 0;
+		scanf("%d",&A[i/m][i%m]);
+	}
+	for(i=0;i<m;i++) {
+		scanf("%d",&b[i]);
+	}
+
+	int ans[n];
+	int j;
+	for(i=0;i<n;i++) {
+		ans[i] = 0;
+		for(j=0;j<m;j++) {
+			ans[i] += A[i][j]*b[j];
+		}
+		printf("%d\n",ans[i]);
+	}
 	return 0;
 }
