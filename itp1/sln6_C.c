61b93166498946b2c6a19bf03bfdecf8b80740b1
Sysak <www.vantanphsh1036@gmail.com>
2018-06-21 22:47:47 +0900

    Implemented input of each data

--- itp1/sln6_C.c
+++ itp1/sln6_C.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+	int in;
+	scanf("%d",&in);
 	
+	int b[in], f[in], r[in], v[in];
+	int i;
+	for(i=0;i<in;i++) {
+		scanf("%d%d%d%d", &b[i], &f[i], &r[i], &v[i]);
+	}
+
+	for(i=0;i<in;i++) {
+		printf("%d, %d, %d, %d\n", b[i], f[i], r[i], v[i]);
+	}
 	return 0;
 }
