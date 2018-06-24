835d0af6790cb36a73bab715cbfddf5286946cbc
Sysak <www.vantanphsh1036@gmail.com>
2018-06-24 20:31:42 +0900

    Probably completed

--- itp1/sln7_B.c
+++ itp1/sln7_B.c
@@ -7,18 +7,17 @@ int main()
 		scanf("%d%d", &n,&x);
 		if(n==0&&x==0) return 0;
 		
-		int i,j,k,c;
+		int i,j,k,c=0;
 		for(i=1;i<=n;i++) {
 			for(j=1;j<=n;j++) {
 				for(k=1;k<=n;k++) {
 					if(i!=j&&j!=k&&i!=k&&i+j+k == x) {
-						printf("%d %d %d\n",i,j,k);
-						//c++;
+						c++;
 					}
 				}
 			}
 		}
-		printf("%d\n",c);
+		printf("%d\n",c/6);
 	}
 	return 0;
 }
