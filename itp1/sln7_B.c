fea779813fa758b12c41bf2cf6b82fcd7de090bb
Sysak <www.vantanphsh1036@gmail.com>
2018-06-24 20:19:05 +0900

    combination v1

--- itp1/sln7_B.c
+++ itp1/sln7_B.c
@@ -6,6 +6,19 @@ int main()
 	while(1) {
 		scanf("%d%d", &n,&x);
 		if(n==0&&x==0) return 0;
+		
+		int i,j,k,c;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=n;j++) {
+				for(k=1;k<=n;k++) {
+					if(i!=j&&j!=k&&i!=k&&i+j+k == x) {
+						printf("%d %d %d\n",i,j,k);
+						//c++;
+					}
+				}
+			}
+		}
+		printf("%d\n",c);
 	}
 	return 0;
 }
