95ab7ed23b379baebbfdca980f590db699a3ae4e
synsax <www.vantanphsh1036@gmail.com>
2018-06-25 01:25:54 +0900

    Done: in sln7_D.c; Implemented all

--- itp1/sln7_D.c
+++ itp1/sln7_D.c
@@ -19,5 +19,20 @@ int main()
             scanf("%d",&B[i][j]);
         }
     }
+    
+    int C=0,k=0,h=0,v=0;
+    for(i=0;i<n;i++) {
+        for(j=0;j<l;j++) {
+            C = 0;
+            for(k=0;k<m;k++) {
+                C += A[v%n][k] * B[k][h%l];
+            }
+            h++;
+            if(j+1==l)printf("%d",C);
+            else printf("%d ",C);
+        }
+        v++;
+        putchar('\n');
+    }
     return 0;
 }
