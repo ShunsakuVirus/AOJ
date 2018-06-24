88983ccd56810d1c27918b9b1c49f7bf4fb0637a
Sysak <www.vantanphsh1036@gmail.com>
2018-06-24 20:40:29 +0900

    Implemented input of each data

--- itp1/sln7_C.c
+++ itp1/sln7_C.c
@@ -2,5 +2,20 @@
 
 int main()
 {
+	int r,c;
+	scanf("%d%d",&r,&c);
+
+	int i,j;
+	int storage[r][c];
+	for(i=0;i<r;i++) {
+		for(j=0;j<c;j++) {
+			scanf("%d",&storage[i][j]);
+		}
+	}
+	for(i=0;i<r;i++) {
+		for(j=0;j<c;j++) {
+			printf("%d",storage[i][j]);
+		}
+	}
 	return 0;
 }
