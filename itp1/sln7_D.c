5b72ab2e3c3acf7a5e0e98c6775ea423da1d470b
synsax <www.vantanphsh1036@gmail.com>
2018-06-24 23:20:33 +0900

    Implement input each of data

--- itp1/sln7_D.c
+++ itp1/sln7_D.c
@@ -2,7 +2,22 @@
 
 int main()
 {
-	int n,m,l;
-	scanf("%d%d%d",&n,&m,&l);
-	return 0;
+    int n,m,l;
+    scanf("%d%d%d",&n,&m,&l);
+    
+    int A[n][m],B[m][l];
+    int i,j;
+    for(i=0;i<n;i++) {
+        for(j=0;j<m;j++) {
+            A[i][j] = 0;
+            scanf("%d",&A[i][j]);
+        }
+    }
+    for(i=0;i<m;i++) {
+        for(j=0;j<l;j++) {
+            B[i][j] = 0;
+            scanf("%d",&B[i][j]);
+        }
+    }
+    return 0;
 }
