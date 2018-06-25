decfe74b8cf3a96ec7cba60219ed8df5cd3f00de
synsax <www.vantanphsh1036@gmail.com>
2018-06-25 15:58:46 +0900

    Fix : Erase comment out

--- itp1/sln8_A.c
+++ itp1/sln8_A.c
@@ -9,19 +9,11 @@ int main()
     int i;
     int offset = 'a'-'A';
     for(i=0;i<MAXCHARS;i++) {
-        //char buff = '\0';
         if(s[i] >= 65 && s[i] <= 90) {
-            //buff = s[i] + offset;
             s[i] += offset;
-            //printf("%c",buff);
         }
         else if(s[i] >= 97 && s[i] <= 122) {
-            //buff = s[i] - offset;
             s[i] -= offset;
-            //printf("%c",buff);
-        }
-        else{
-            //printf("%c",s[i]);
         }
     }
     printf("%s",s);
