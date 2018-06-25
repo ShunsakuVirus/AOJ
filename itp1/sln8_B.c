9dee71b173619220ccd80d69251ea8533d9fbcea
synsax <www.vantanphsh1036@gmail.com>
2018-06-25 19:40:10 +0900

    Done All : in sln8_B.c

---
+++ itp1/sln8_B.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define MAXDIGITS 1000
+
+int main()
+{
+    char x[MAXDIGITS+1];
+    int i;
+    for(i=0;i<MAXDIGITS+1;i++) {
+        x[i] = '\0';
+    }
+    while(1) {
+        fgets(x,MAXDIGITS,stdin);
+        if(x[0] == '0') return 0;
+        int buff = 0;
+        for(i=0;i<MAXDIGITS;i++) {
+            if(x[i] != '\n' && x[i] != '\0') {
+                buff += ((int)x[i])-48;
+            }
+        }
+        printf("%d\n",buff);
+    }
+    return 0;
+}
