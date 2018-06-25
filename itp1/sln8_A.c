7bdd517ec55dd1de16545b64f2956408d44f9777
synsax <www.vantanphsh1036@gmail.com>
2018-06-25 15:56:08 +0900

    Done All : in sln8_A.c Implement all

--- itp1/sln8_A.c
+++ itp1/sln8_A.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
-#define MAX_CHAR 1200
+#define MAXCHARS 1200
 
 int main()
 {
-    char c[MAX_CHAR+1];
-    scanf("%s",c);
-
+    char s[MAXCHARS+1];
+    fgets(s, MAXCHARS, stdin);
+    
+    int i;
+    int offset = 'a'-'A';
+    for(i=0;i<MAXCHARS;i++) {
+        //char buff = '\0';
+        if(s[i] >= 65 && s[i] <= 90) {
+            //buff = s[i] + offset;
+            s[i] += offset;
+            //printf("%c",buff);
+        }
+        else if(s[i] >= 97 && s[i] <= 122) {
+            //buff = s[i] - offset;
+            s[i] -= offset;
+            //printf("%c",buff);
+        }
+        else{
+            //printf("%c",s[i]);
+        }
+    }
+    printf("%s",s);
     return 0;
 }
