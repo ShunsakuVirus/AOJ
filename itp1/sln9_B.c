30626e997a640534c8719559ce04ecd4d09c35d6
synsax <32933169+synsax@users.noreply.github.com>
2018-08-04 01:02:26 +0900

    update: sln9_B

--- itp1/sln9_B.c
+++ itp1/sln9_B.c
@@ -2,6 +2,40 @@
 
 #define MAXCHARS 200
 
+void shuffle(char s[MAXCHARS], int n) {
+    char buff[n+1];
+    int i;
+    for(i=0;i<n;i++) {
+        buff[i] = s[i];
+    }
+    buff[n] = 0;
+    for(i=0;i<MAXCHARS;i++) {
+        int flag = s[i+n]==0|s[i+n]==10|(i+n)>=MAXCHARS;
+        if(flag) {
+            puts("Out of range.");
+            s[i+n] = 0;
+            break;
+        }
+        s[i] = s[i+n];
+        s[i+n] = 0;
+    }
+    int end = 0;
+    while(s[end] != 0) {
+        end++;
+    }
+    for(i=0;i<n;i++) {
+        s[end+i] = buff[i];
+    }
+    printf("buff = %s\n",buff);
+    for(i=0;i<n;i++) {
+        printf("[%d] \n",buff[i]);
+    }
+    printf("s = %s\n",s);
+    for(i=0;i<MAXCHARS;i++){
+        printf("s[%d] = %d\n",i,s[i]);
+    }
+}
+
 int main()
 {
     char deck[MAXCHARS+1];
